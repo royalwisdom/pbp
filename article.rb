2e99c5a8923a5d6062f89cc68dfbcffb750e7c4a
royalwisdom <roxymarrese@gmail.com>
2017-02-08 15:20:20 -0600

    Update article.rb

--- article.rb
+++ article.rb
@@ -1,6 +1,28 @@
 class Article < ApplicationRecord
   validates :title, :preview, :text, presence: true
   
+  def self.create_article(submitted_article)
+    body = submitted_article['body']
+    body_length = body.length
+    preview = []
+    i = 0
+    j = 0
     
+    body.each{ |t|
+    while i < 3
+      if body[j] != "."
+        preview << body[j]
+      else
+        preview << body[j]
+        i = i + 1
+      end
+    end
+    
+    article = Article.new do |a|
+      a.title = submitted_article['title']
+      a.preview = submitted_article['preview']
+      a.body = submitted_article['body']
+      end
+  end 
   
 end
